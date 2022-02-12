c818bb2b07650eb2ab185c024dd2b109f1ea086e
Atharv-777 <atharvbidwe11@gmail.com>
2022-02-12 15:52:26 +0530

    added on/off property

--- js/index.js
+++ js/index.js
@@ -4,12 +4,26 @@ let header = document.getElementById('header');
 let click = document.getElementById('click');
 let textShadow = document.getElementsByClassName('shadow')
 
+let on = false
+
 bulb.onclick = function(){
-    bulb.style.background = 'rgb(36,0,29)';
-    bulb.style.background = 'linear-gradient(90deg, rgba(36,0,29,1) 0%, rgba(121,9,64,1) 48%, rgba(0,52,255,1) 100%)';
-    header.style.color = 'white';
-    text[0].style.color = 'white';
-    text[0].style.animation = 'glow 1s ease-in-out infinite alternate';
-    click.style.display = 'none';
-    textShadow[0].style.animation = 'shadowGlow 1s ease-in-out infinite alternate'
+    if(on == false){
+        bulb.style.background = 'rgb(36,0,29)';
+        bulb.style.background = 'linear-gradient(90deg, rgba(36,0,29,1) 0%, rgba(121,9,64,1) 48%, rgba(0,52,255,1) 100%)';
+        header.style.color = 'white';
+        text[0].style.color = 'white';
+        text[0].style.animation = 'glow 1s ease-in-out infinite alternate';
+        click.style.display = 'none';
+        textShadow[0].style.animation = 'shadowGlow 1s ease-in-out infinite alternate'
+        on = true
+    }
+    else{
+        bulb.style.background = 'rgb(46, 46, 45)';
+        header.style.color = 'color: rgb(90, 90, 90);';
+        text[0].style.color = 'rgb(43, 43, 43)';
+        text[0].style.animation = 'none';
+        click.style.display = 'block';
+        textShadow[0].style.animation = 'none'
+        on = false
+    }
 }
