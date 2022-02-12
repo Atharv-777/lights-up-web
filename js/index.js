54e43cfae314f857b3987e5493f4cba8e38d5a98
Atharv-777 <atharvbidwe11@gmail.com>
2022-02-12 17:58:15 +0530

    added footer

--- js/index.js
+++ js/index.js
@@ -3,27 +3,32 @@ let text = document.getElementsByClassName('text')
 let header = document.getElementById('header');
 let click = document.getElementById('click');
 let textShadow = document.getElementsByClassName('shadow')
+let downloadBtn = document.getElementsByClassName('downloadBtn')
 
 let on = false
+
+function changeStyle(headerCol, textCol, textAnim, clickDis, textShAnim, dBtnVis){
+
+    header.style.color = headerCol;
+    text[0].style.color = textCol;
+    text[0].style.animation = textAnim;
+    click.style.display = clickDis;
+    textShadow[0].style.animation = textShAnim;
+    downloadBtn[0].style.visibility = dBtnVis;
+}
+
 
 bulb.onclick = function(){
     if(on == false){
         bulb.style.background = 'rgb(36,0,29)';
-        bulb.style.background = 'linear-gradient(90deg, rgba(36,0,29,1) 0%, rgba(121,9,64,1) 48%, rgba(0,52,255,1) 100%)';
-        header.style.color = 'white';
-        text[0].style.color = 'white';
-        text[0].style.animation = 'glow 1s ease-in-out infinite alternate';
-        click.style.display = 'none';
-        textShadow[0].style.animation = 'shadowGlow 1s ease-in-out infinite alternate'
-        on = true
+        bulb.style.background = 'linear-gradient(158deg, rgba(36,0,29,1) 0%, rgba(121,9,64,1) 48%, rgba(0,52,255,1) 100%)';
+        changeStyle('white', 'white', 'glow 1s ease-in-out infinite alternate', 'none', 'shadowGlow 1s ease-in-out infinite alternate', 'visible')
+
+        on = true;
     }
     else{
         bulb.style.background = 'rgb(46, 46, 45)';
-        header.style.color = 'color: rgb(90, 90, 90);';
-        text[0].style.color = 'rgb(43, 43, 43)';
-        text[0].style.animation = 'none';
-        click.style.display = 'block';
-        textShadow[0].style.animation = 'none'
+        changeStyle('rgb(90, 90, 90)', 'rgb(19, 19, 19)', 'none', 'block', 'none', 'hidden');
         on = false
     }
 }
